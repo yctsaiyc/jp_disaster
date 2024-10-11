ce2e53d2d49b9e7956bd9dcd71825cd08112554e
yctsaiyc <yctsaiyc@gmail.com>
2024-10-11 14:03:09 +0800

    feat: save data xml

--- egov.py
+++ egov.py
@@ -55,10 +55,37 @@ def get_data_urls():
     return urls
 
 
+def save_data_xml(urls):
+    for feed in urls:
+        for url in urls[feed]:
+            xml_name = url.split("/")[-1]
+            code = xml_name.split("_")[-2]
+            time = xml_name.split("_")[-4]
+
+            dir_path = os.path.join(config["data_dir"], "data", code, "xml")
+            os.makedirs(dir_path, exist_ok=True)
+            xml_path = os.path.join(dir_path, xml_name)
+
+            if os.path.exists(xml_path):
+                print("exists", xml_path)
+                break
+
+            else:
+                response = requests.get(url)
+                response.encoding = "utf-8"
+                xml = response.text
+
+                with open(xml_path, "w") as file:
+                    file.write(xml)
+
+                print("saved", xml_path)
+
+
 def main():
     config = load_config("egov.json")
-    # save_feed_xml()
+    save_feed_xml()
     urls = get_data_urls()
+    save_data_xml(urls)
 
 
 if __name__ == "__main__":
