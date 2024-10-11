4941590065f08de387e5621d4e665cb25acab5cc
yctsaiyc <yctsaiyc@gmail.com>
2024-10-11 11:41:21 +0800

    feat: get data urls

--- egov.py
+++ egov.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import os
+from bs4 import BeautifulSoup
 
 
 config = {}
@@ -17,15 +18,15 @@ def save_feed_xml():
     dir_path = os.path.join(config["data_dir"], "feed")
     os.makedirs(dir_path, exist_ok=True)
 
-    for name in config["feed_urls"]:
-        url = config["feed_urls"][name]
+    for feed in config["feed_urls"]:
+        url = config["feed_urls"][feed]
         print("url", url)
 
         response = requests.get(url)
-        response.encoding = 'utf-8'
+        response.encoding = "utf-8"
         xml = response.text
 
-        xml_path = os.path.join(dir_path, f"{name}.xml")
+        xml_path = os.path.join(dir_path, f"{feed}.xml")
 
         with open(xml_path, "w") as file:
             file.write(xml)
@@ -35,9 +36,29 @@ def save_feed_xml():
     return
 
 
+def get_data_urls():
+    feed_dir_path = os.path.join(config["data_dir"], "feed")
+    urls = {}
+
+    for feed in config["feed_urls"]:
+        urls[feed] = []
+        xml_path = os.path.join(feed_dir_path, f"{feed}.xml")
+
+        with open(xml_path, "r", encoding="utf-8") as file:
+            content = file.read()
+
+        soup = BeautifulSoup(content, "xml")
+
+        for entry in soup.find_all("entry"):
+            urls[feed].append(entry.find("id").text)
+
+    return urls
+
+
 def main():
     config = load_config("egov.json")
-    save_feed_xml()
+    # save_feed_xml()
+    urls = get_data_urls()
 
 
 if __name__ == "__main__":
