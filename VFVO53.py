4dc33f17d8f478c3e2c7c00bafef2e2c214bc22b
yctsaiyc <yctsaiyc@gmail.com>
2024-10-15 16:16:46 +0800

    refactor: VFVO53

--- VFVO53.py
+++ VFVO53.py
@@ -6,71 +6,79 @@ import pandas as pd
 
 
 class ETL_VFVO53(ETL_jp_disaster):
+    # 降灰予報（対象火山）
+    def volcano_info_1_to_df(self, soup, rows_count=1):
+        volcano_info = soup.find("VolcanoInfo", {"type": "降灰予報（対象火山）"})
+        kind_name = volcano_info.find("Kind").find("Name").text
+
+        area = volcano_info.find("Area")
+        area_name = area.find("Name").text
+
+        coordinate = area.find("Coordinate").text.replace("/", "").split("+")
+
+        latitude = self.dms_to_decimal(float(coordinate[1]))
+        longitude = self.dms_to_decimal(float(coordinate[2]))
+        height = float(coordinate[3])
+
+        df = pd.DataFrame(
+            {
+                "Kind": [kind_name] * rows_count,
+                "Area": [area_name] * rows_count,
+                "Latitude": [latitude] * rows_count,
+                "Longitude": [longitude] * rows_count,
+                "Height": [height] * rows_count,
+            }
+        )
+
+        return df
+
+    # 降灰予報（対象市町村等）
+    def volcano_info_2_to_df(self, soup):
+        volcano_info = soup.find("VolcanoInfo", {"type": "降灰予報（対象市町村等）"})
+
+        df = pd.DataFrame(columns=["Kind", "Area"])
+
+        for item in volcano_info.find_all("Item"):
+            kind_name = item.find("Kind").find("Name").text
+
+            areas = item.find_all("Area")
+            area_names = [area.find("Name").text for area in areas]
+
+            df = pd.concat(
+                [
+                    df,
+                    pd.DataFrame(
+                        {
+                            "Kind": [kind_name] * len(area_names),
+                            "Area": area_names,
+                        }
+                    ),
+                ],
+                ignore_index=True,
+            )
+
+        return df
+
     def xml_to_df(self, xml_path, soup):
         try:
             df = pd.DataFrame(columns=self.columns)
 
-            volcano_info_1 = soup.find("VolcanoInfo", {"type": "降灰予報（対象火山）"})
-            volcano_info_2 = soup.find(
-                "VolcanoInfo", {"type": "降灰予報（対象市町村等）"}
-            )
-            ash_infos = soup.find_all("AshInfo", {"type": "降灰予報（定時）"})
+            # 降灰予報（対象市町村等）
+            df_volcano_info_2 = self.volcano_info_2_to_df(soup)
 
             # 降灰予報（対象火山）
-            kind_1_name = volcano_info_1.find("Kind").find("Name").text
-
-            area_1 = volcano_info_1.find("Area")
-            area_1_name = area_1.find("Name").text
-
-            coordinate = area_1.find("Coordinate").text.replace("/", "").split("+")
-
-            latitude = self.dms_to_decimal(float(coordinate[1]))
-            longitude = self.dms_to_decimal(float(coordinate[2]))
-            height = float(coordinate[3])
-
-            df1 = pd.DataFrame(
-                {
-                    "Kind": [kind_1_name],
-                    "Area": [area_1_name],
-                    "Latitude": [latitude],
-                    "Longitude": [longitude],
-                    "Height": [height],
-                }
+            df_volcano_info_1 = self.volcano_info_1_to_df(
+                soup, rows_count=len(df_volcano_info_2)
             )
 
-            # 降灰予報（対象市町村等）
-            df2 = pd.DataFrame(columns=["Kind", "Area"])
-
-            for item in volcano_info_2.find_all("Item"):
-                kind_2_name = item.find("Kind").find("Name").text
-
-                area_2s = item.find_all("Area")
-                area_2_names = [area_2.find("Name").text for area_2 in area_2s]
-
-                kind_2_names = [kind_2_name] * len(area_2_names)
-
-                df2 = pd.concat(
-                    [
-                        df2,
-                        pd.DataFrame(
-                            {
-                                "Kind": kind_2_names,
-                                "Area": area_2_names,
-                            }
-                        ),
-                    ],
-                    ignore_index=True,
-                )
-
-            print(
-                pd.concat(
-                    [pd.concat([df1] * len(df2), ignore_index=True), df2],
-                    axis=1,
-                    ignore_index=True,
-                )
+            df = pd.concat(
+                [df_volcano_info_1, df_volcano_info_2], axis=1, ignore_index=True
             )
-
+            print(df)
             exit()
+
+            # 降灰予報（定時）
+            ash_infos = soup.find_all("AshInfo", {"type": "降灰予報（定時）"})
 
             # Save DataFrame to CSV
             self.df_to_csv(df, xml_path)
