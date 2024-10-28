21ad0b73a1d208fb7d5a1854dd2f2f6a4cfec6e6
yctsaiyc <yctsaiyc@gmail.com>
2024-10-28 16:24:13 +0800

    refactor: restructure save_xml into a class

--- disaster.py
+++ disaster.py
@@ -6,92 +6,81 @@ import glob
 import shutil
 
 
-config = {}
+class XML_saver:
+    def __init__(self, config_path):
+        with open(config_path) as file:
+            self.config = json.load(file)
 
-
-def load_config(config_path):
-    global config
-
-    with open(config_path) as file:
-        config = json.load(file)
-
-
-def save_feed_xml():
-    for feed in config["feed_urls"]:
-        dir_path = os.path.join(config["data_dir"], feed)
-        os.makedirs(dir_path, exist_ok=True)
-
-        url = config["feed_urls"][feed]
-        print("URL:", url)
-
-        response = requests.get(url)
-        response.encoding = "utf-8"
-        xml = response.text
-
-        xml_path = os.path.join(dir_path, f"{feed}.xml")
-
-        with open(xml_path, "w") as file:
-            file.write(xml)
-
-        print("Saved:", xml_path)
-
-    return
-
-
-def get_data_urls():
-    urls = {}
-
-    for feed in config["feed_urls"]:
-        urls[feed] = []
-        xml_path = os.path.join(config["data_dir"], feed, f"{feed}.xml")
-
-        with open(xml_path, "r", encoding="utf-8") as file:
-            content = file.read()
-
-        soup = BeautifulSoup(content, "xml")
-
-        for entry in soup.find_all("entry"):
-            urls[feed].append(entry.find("id").text)
-
-    return urls
-
-
-def save_data_xml(urls):
-    for feed in urls:
-        for url in urls[feed]:
-            xml_name = url.split("/")[-1]
-            code = xml_name.split("_")[-2]
-            time = xml_name.split("_")[-4]
-
-            dir_path = os.path.join(config["data_dir"], feed, code, "xml")
+    def save_feed_xml(self):
+        for feed in self.config["feed_urls"]:
+            dir_path = os.path.join(self.config["data_dir"], feed)
             os.makedirs(dir_path, exist_ok=True)
-            xml_path = os.path.join(dir_path, xml_name)
 
-            if os.path.exists(xml_path) or os.path.exists(
-                os.path.join(xml_path, "converted")
-            ):
-                print("Exists:", xml_path)
-                break
+            url = self.config["feed_urls"][feed]
+            print("URL:", url)
 
-            else:
-                response = requests.get(url)
-                response.encoding = "utf-8"
-                xml = response.text
+            response = requests.get(url)
+            response.encoding = "utf-8"
+            xml = response.text
 
-                soup = BeautifulSoup(xml, "xml")
-                pretty_xml = soup.prettify()
+            xml_path = os.path.join(dir_path, f"{feed}.xml")
 
-                with open(xml_path, "w") as file:
-                    file.write(pretty_xml)
+            with open(xml_path, "w") as file:
+                file.write(xml)
 
-                print("Saved:", xml_path)
+            print("Saved:", xml_path)
 
+    def get_data_urls(self):
+        urls = {}
 
-def main(config_path):
-    config = load_config(config_path)
-    save_feed_xml()
-    urls = get_data_urls()
-    save_data_xml(urls)
+        for feed in self.config["feed_urls"]:
+            urls[feed] = []
+            xml_path = os.path.join(self.config["data_dir"], feed, f"{feed}.xml")
+
+            with open(xml_path, "r", encoding="utf-8") as file:
+                content = file.read()
+
+            soup = BeautifulSoup(content, "xml")
+
+            for entry in soup.find_all("entry"):
+                urls[feed].append(entry.find("id").text)
+
+        return urls
+
+    def save_data_xml(self, urls):
+        for feed in urls:
+            for url in urls[feed]:
+                xml_name = url.split("/")[-1]
+                code = xml_name.split("_")[-2]
+                time = xml_name.split("_")[-4]
+
+                dir_path = os.path.join(self.config["data_dir"], feed, code, "xml")
+                os.makedirs(dir_path, exist_ok=True)
+                xml_path = os.path.join(dir_path, xml_name)
+
+                if os.path.exists(xml_path) or os.path.exists(
+                    os.path.join(xml_path, "converted")
+                ):
+                    print("Exists:", xml_path)
+                    break
+
+                else:
+                    response = requests.get(url)
+                    response.encoding = "utf-8"
+                    xml = response.text
+
+                    soup = BeautifulSoup(xml, "xml")
+                    pretty_xml = soup.prettify()
+
+                    with open(xml_path, "w") as file:
+                        file.write(pretty_xml)
+
+                    print("Saved:", xml_path)
+
+    def save_xml(self):
+        self.save_feed_xml()
+        urls = self.get_data_urls()
+        self.save_data_xml(urls)
 
 
 class ETL_jp_disaster:
@@ -148,4 +137,5 @@ class ETL_jp_disaster:
 
 
 if __name__ == "__main__":
-    main("disaster.json")
+    xml_saver = XML_saver("disaster.json")
+    xml_saver.save_xml()
