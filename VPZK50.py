439789e13a116594226657c8d83d42ed90d6896f
yctsaiyc <yctsaiyc@gmail.com>
2024-11-11 16:31:29 +0800

    feat: VPZK50

---
+++ VPZK50.py
@@ -0,0 +1,15 @@
+from VPCK50 import ETL_VPCK50
+import pandas as pd
+
+
+# 地方季節予報 (全般１か月予報，全般３か月予報，全般暖・寒候期予報)
+# 気象庁防災情報XMLフォーマット　技術資料: https://xml.kishou.go.jp/tec_material.html
+# 電文毎の解説資料: https://xml.kishou.go.jp/jmaxml_20241031_Manual(pdf).zip
+class ETL_VPZK50(ETL_VPCK50):
+    pass
+
+
+if __name__ == "__main__":
+    config_path = "disaster.json"
+    etl_vpzk50 = ETL_VPZK50(config_path, "regular", "VPZK50")
+    etl_vpzk50.xml_to_csv()
