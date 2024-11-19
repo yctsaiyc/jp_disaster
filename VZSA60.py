94bc0d547c9a2f0597b4b1222cbeb3cb4ebf4ffb
yctsaiyc <yctsaiyc@gmail.com>
2024-11-19 18:37:36 +0800

    feat: VZSA60

--- VZSA60.py
+++ VZSA60.py
@@ -2,16 +2,16 @@ from VZSF51 import ETL_VZSF51
 import pandas as pd
 
 
-# 地上実況図
+# アジア太平洋地上実況図
 # 気象庁防災情報XMLフォーマット　技術資料: https://xml.kishou.go.jp/tec_material.html
 # 電文毎の解説資料: https://xml.kishou.go.jp/jmaxml_20241031_Manual(pdf).zip
 
 
-class ETL_VZSA50(ETL_VZSF51):
+class ETL_VZSA60(ETL_VZSF51):
     pass
 
 
 if __name__ == "__main__":
     config_path = "disaster.json"
-    etl_vzsa50 = ETL_VZSA50(config_path, "regular", "VZSA50")
-    etl_vzsa50.xml_to_csv()
+    etl_vzsa60 = ETL_VZSA60(config_path, "regular", "VZSA60")
+    etl_vzsa60.xml_to_csv()
