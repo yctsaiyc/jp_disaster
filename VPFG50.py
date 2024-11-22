8acaec5ce25f23d4c113826fa3e795b5ec6c0b77
yctsaiyc <yctsaiyc@gmail.com>
2024-11-22 17:25:24 +0800

    feat: VPFG50

---
+++ VPFG50.py
@@ -0,0 +1,21 @@
+from disaster import ETL_jp_disaster
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+# 府県天気概況
+# 気象庁防災情報XMLフォーマット　技術資料: https://xml.kishou.go.jp/tec_material.html
+# 電文毎の解説資料: https://xml.kishou.go.jp/jmaxml_20241031_Manual(pdf).zip
+
+
+class ETL_VPFG50(ETL_jp_disaster):
+    def xml_to_df(self, xml_path, soup):
+        df = pd.DataFrame(columns=self.columns)
+
+        return df
+
+
+if __name__ == "__main__":
+    config_path = "disaster.json"
+    etl_vpfg50 = ETL_VPFG50(config_path, "regular", "VPFG50")
+    etl_vpfg50.xml_to_csv()
