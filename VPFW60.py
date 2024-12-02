a9ea5a264bc553179a8d12afd8219e3c37981672
yctsaiyc <yctsaiyc@gmail.com>
2024-12-02 13:56:30 +0800

    chore: VPFW60

--- VPFW60.py
+++ VPFW60.py
@@ -3,41 +3,28 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
+# 警報級の可能性（明後日以降）
+# 気象庁防災情報XMLフォーマット　技術資料: https://xml.kishou.go.jp/tec_material.html
+# 電文毎の解説資料: https://xml.kishou.go.jp/jmaxml_20241031_Manual(pdf).zip
+
+
 class ETL_VPFW60(ETL_jp_disaster):
-    def parse_TimeDefines(self, TimeDefines):
-        DateTime_dict = {}
-        Name_dict = {}
-
-        for TimeDefine in TimeDefines.find_all("TimeDefine"):
-            timeId = TimeDefine.get("timeId")
-
-            DateTime_dict[timeId] = self.format_datetime(
-                TimeDefine.find("DateTime").text
-            )
-
-            name = TimeDefine.find("Name")
-
-            if name:
-                Name_dict[timeId] = name.text
-
-        if not Name_dict:
-            return DateTime_dict
-
-        return DateTime_dict, Name_dict
-
     def xml_to_df(self, xml_path, soup):
         df = pd.DataFrame(columns=self.columns)
 
         # 2 各部の構成と内容
 
         # (1) 管理部
+
         # 1 管理部(Control)の構成と内容
 
         # Control
-        # └Title 情報名称 電文の種別を示すための情報名称を示す。“警報級の可能性(明後日以降)”で固定。
-        # └DateTime 発表時刻 発表時刻。未来時刻にはならない。
-        #     “2008-06-26T01:51:00Z”のように協定世界時で記述する。
-        # └Status 運用種別 本情報の位置づけ。“通常”“訓練”“試験”のいずれかを記載。
+        # └Title 情報名称
+        #     電文の種別を示すための情報名称を示す。“警報級の可能性(明後日以降)”で固定。
+        # └DateTime 発表時刻
+        #     発表時刻。未来時刻にはならない。“2008-06-26T01:51:00Z”のように協定世界時で記述する。
+        # └Status 運用種別
+        #     本情報の位置づけ。“通常”“訓練”“試験”のいずれかを記載。
         #     “訓練”“試験”は正規の情報として利用してはならないことを示す。
         # └EditorialOffice 編集官署名
         #     実際に発表作業を行った「編集官署名」を示す。“気象庁本庁”“大阪管区気象台”のように記述する。
@@ -45,42 +32,51 @@ class ETL_VPFW60(ETL_jp_disaster):
         #     本情報を業務的に発表した「発表官署名」を示す。“気象庁”“大阪管区気象台”のように記述する。
 
         # (2) ヘッダ部
+
         # 1 ヘッダ部(Head)の構成と内容
 
         # Head
-        # └Title 標題 情報を示す標題。具体的な内容が判別できる名称であり、可視化を目的として利用する。
-        #   “○○警報級の可能性(明後日以降)”(○○は府県予報区名)と記述する。
+        # └Title 標題
+        #     情報を示す標題。具体的な内容が判別できる名称であり、可視化を目的として利用する。
+        #     “○○警報級の可能性(明後日以降)”(○○は府県予報区名)と記述する。
 
-        # └ReportDateTime 発表時刻 本情報の公式な発表時刻を示す。“2008-06-26T11:00:00+09:00”のように日本標準時で記述する。
+        # └ReportDateTime 発表時刻
+        #     本情報の公式な発表時刻を示す。“2008-06-26T11:00:00+09:00”のように日本標準時で記述する。
         ReportDateTime = self.format_datetime(soup.find("ReportDateTime").text)
 
-        # └TargetDateTime 基点時刻 本情報の対象となる時刻・時間帯の基点時刻を示す。
-        #     “2008-06-28T00:00:00+09:00”のように日本標準時で記述する。
+        # └TargetDateTime 基点時刻
+        #     本情報の対象となる時刻・時間帯の基点時刻を示す。“2008-06-28T00:00:00+09:00”のように日本標準時で記述する。
         TargetDateTime = self.format_datetime(soup.find("TargetDateTime").text)
 
         # └TargetDuration 基点時刻からの取りうる時間
         #     情報の対象が時間幅を持つ場合、TargetDateTime を基点とした時間の幅を示す。
         #     “P4D”で、「基点時刻」から 4 日先までの予報であることを示す。
-        # └EventID 識別情報 警報級の可能性(明後日以降)では要素内に何も記述しない。
-        # └InfoType 情報形態 本情報の形態を示す。“発表”“訂正”“遅延”のいずれかを記述する。
-        # └Serial 情報番号 警報級の可能性(明後日以降)では要素内に何も記述しない。
+        # └EventID 識別情報
+        #     警報級の可能性(明後日以降)では要素内に何も記述しない。
+        # └InfoType 情報形態
+        #     本情報の形態を示す。“発表”“訂正”“遅延”のいずれかを記述する。
+        # └Serial 情報番号
+        #     警報級の可能性(明後日以降)では要素内に何も記述しない。
         # └InfoKind スキーマの運用種別情報
         #     同一スキーマ上における情報分類に応じた運用を示す種別情報である。
         #     “警報級の可能性(明後日以降)”と記述する。
         # └InfoKindVersion スキーマの運用種別情報のバージョン
         #     スキーマの運用種別情報におけるバージョン番号を示す。本解説のバージョン番号は“1.2_0”。
-        # └Headline 見出し要素 防災気象情報事項となる見出し要素を示す。警報級の可能性(明後日以降)では何も記述しない。
-        #     └ Text 見出し文 警報級の可能性(明後日以降)では要素内に何も記述しない。
+        # └Headline 見出し要素
+        #     防災気象情報事項となる見出し要素を示す。警報級の可能性(明後日以降)では何も記述しない。
+        #     └ Text 見出し文
+        #         警報級の可能性(明後日以降)では要素内に何も記述しない。
 
         # (3) 内容部
         # 1 内容部(Body)の構成と内容
 
         # Body
         # └MeteorologicalInfos 予報の項目
-        #      警報級の気象現象が発生する可能性を記述する。
+        #     警報級の気象現象が発生する可能性を記述する。
         MeteorologicalInfos = soup.find("MeteorologicalInfos")
 
-        #     └ TimeSeriesInfo 時系列情報 MeteorologicalInfos の予報を時系列情報として記述する。
+        # └ TimeSeriesInfo 時系列情報
+        #     MeteorologicalInfos の予報を時系列情報として記述する。
 
         # 2内容部の個別要素の詳細
 
